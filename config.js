356ef9897b522eb1de6a95b38439ec6994d6052f
Surik95 <pokemon.surik@gmail.com>
2023-03-10 11:26:28 +0300

    Добавил Default значения

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  tokenWeather: process.env.TOKKEN_WEATHERSTACK,
-  urlWeatherstack: process.env.URL_WEATHERSTACK,
+  tokenWeather: process.env.TOKKEN_WEATHERSTACK
+    ? process.env.TOKKEN_WEATHERSTACK
+    : 'd5e70b172af746900ea7a99c46149159',
+  urlWeatherstack: process.env.URL_WEATHERSTACK
+    ? process.env.URL_WEATHERSTACK
+    : 'http://api.weatherstack.com/current?',
   region: 'Moscow',
 };
